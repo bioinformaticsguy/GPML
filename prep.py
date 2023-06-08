1492e12709789da0aaf8e9ebb0edb87c7fb9a884
bioinformaticsguy <bioinformaticsguy@gmail.com>
2023-06-08 18:24:59 +0200

    minor updates in the code

--- prep.py
+++ prep.py
@@ -1,22 +1,40 @@
-s1, s2, s3, s4 = 0,0,0,0 
+# s1, s2, s3, s4 = 0,0,0,0 
 
-with open('Protein_mave_db_gold_standard_only_sequences_splits.tsv') as file:
+# with open('Protein_mave_db_gold_standard_only_sequences_splits.tsv') as file:
+#     for line in file:
+#         if line[-8:] == 'Split_1\n':
+#             s1+=1
+#             # print(line)
+#         if line[-8:] == 'Split_2\n':
+#             s2+=1
+#             # print(line)
+#         if line[-8:] == 'Split_3\n':
+#             s3+=1
+#             # print(line)
+#         if line[-8:] == 'Split_4\n':
+#             s4+=1
+#             # print(line)
+
+
+# print(s1, s2, s3, s4)
+
+
+# ## Testing line
+
+
+data_directory = "/home/ali/Documents/GPML/Data/"
+
+mave_folder_name = "maveGSData/"
+
+file_name = "mave_db_gold_standard.fasta"
+
+file_path = data_directory + mave_folder_name + file_name
+
+list = []
+with open(file_path) as file:
     for line in file:
-        if line[-8:] == 'Split_1\n':
-            s1+=1
-            # print(line)
-        if line[-8:] == 'Split_2\n':
-            s2+=1
-            # print(line)
-        if line[-8:] == 'Split_3\n':
-            s3+=1
-            # print(line)
-        if line[-8:] == 'Split_4\n':
-            s4+=1
-            # print(line)
+        if line[0] == ">":
+            list.append(line)
+            # print(line[:-1])
 
-
-print(s1, s2, s3, s4)
-
-
-## Testing line
+print(len(list))
