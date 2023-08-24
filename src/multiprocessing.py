1dd4572314185c18d2bbab02a8376b27d36a63b0
bioinformaticsguy <bioinformaticsguy@gmail.com>
2023-08-24 02:58:32 +0200

    Calculated the score for the NUTD and reformated the main file.

---
+++ src/multiprocessing.py
@@ -0,0 +1,16 @@
+import subprocess
+import multiprocessing
+
+# Function to run the command for a given input and output file
+def run_command(input_file, output_file):
+    command = f"./run_mutpred2.sh -i {input_file} -p 1 -c 1 -b 0 -t 0.1 -f 4 -o {output_file}"
+    subprocess.call(command, shell=True)
+
+if __name__ == "__main__":
+    input_files = ["t_36_seq.fasta"] * 36  # Assuming you want to use the same input file for all cores
+    output_files = ["t_36_seq.out"] * 36  # Assuming you want to use the same output file for all cores
+
+    pool = multiprocessing.Pool(processes=36)  # Number of cores
+    pool.starmap(run_command, zip(input_files, output_files))
+    pool.close()
+    pool.join()
