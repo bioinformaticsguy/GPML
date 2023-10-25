06f3c0498fe642d32ccab093194b3ace16d7ce47
bioinformaticsguy <bioinformaticsguy@gmail.com>
2023-10-25 16:44:14 +0200

    Added the output files from db_NSFP database.

--- main_dbNSFP.py
+++ main_dbNSFP.py
@@ -21,10 +21,9 @@ uniprot_id_list_of_human_genes = df[df['Organism'] == desired_value]['Uniprot ID
 all_protein_names = list(InputGenerator.get_protein_names_and_snps_from_mutepred_input_file(MUTEPRED_INPUT_FILE_PATH).keys())
 
 
+dict = InputGenerator.get_protein_names_and_snps_from_mutepred_input_file(MUTEPRED_INPUT_FILE_PATH)
+[print(i, len(dict[i])) for i in dict.keys()]
 
-
-
-
-InputGenerator.write_input_files(mutepred_input_file_path = MUTEPRED_INPUT_FILE_PATH,
-                                 genes_meta_data_file_path = META_DATA_INPUT_FILE_PATH,
-                                 dbNSFP_input_files_directory = dbNSFP_INPUT_FILES_DIRECTORY)
+# InputGenerator.write_input_files(mutepred_input_file_path = MUTEPRED_INPUT_FILE_PATH,
+#                                  genes_meta_data_file_path = META_DATA_INPUT_FILE_PATH,
+#                                  dbNSFP_input_files_directory = dbNSFP_INPUT_FILES_DIRECTORY)
