9ce2749b9c3a6ac2013c0d963583c3ab24348815
bioinformaticsguy <bioinformaticsguy@gmail.com>
2023-12-21 15:59:05 +0100

    moved MAVE_GS_DATAFRAME to the main file.

--- main_mutepred_overlap.py
+++ main_mutepred_overlap.py
@@ -1,17 +1,12 @@
 from pathlib import Path
-from main import MAVE_GS_FILE_PATH
 from src.mutepred_overlaps import MutepredDataPreprocessor
-from src.utils import COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST
-from src.dataframe_preprocessor import MAVE_GOLD_STANDARD
+from main_pandas_dataframes import MAVE_GS_DATAFRAME
 
 TRAINING_DATA_FILE_PATH = Path("Data/mutepred_training_data/wo_exclusive_hgmd_mp2_training_data_MavedbData.csv")
 AMINO_ACID_SEQUENCE_SUBSTITUTIONS_COLUMN_NAME = 'Amino_acid_substitutions'
 AMINO_ACID_SEQUENCE_COLUMN_NAME = 'Prot_sequence'
 
 MUTEPRED_DATAFRAME = MutepredDataPreprocessor.get_mutepred_df(TRAINING_DATA_FILE_PATH)
-MAVE_GS_DATAFRAME = MAVE_GOLD_STANDARD.get_dataframe_for_mave_gs_data(MAVE_GS_FILE_PATH,
-                                                   column_names=COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST)
-
 
 
 filtered_data = MutepredDataPreprocessor. \
---
+++ main_pandas_dataframes.py
@@ -0,0 +1,8 @@
+from src.dataframe_preprocessor import MAVE_GOLD_STANDARD
+from main import MAVE_GS_FILE_PATH
+from src.utils import COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST
+
+
+MAVE_GS_DATAFRAME = MAVE_GOLD_STANDARD.get_dataframe_for_mave_gs_data(MAVE_GS_FILE_PATH,
+                                                   column_names=COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST)
+
