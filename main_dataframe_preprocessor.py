1e78bc22f5f7dad32e5ce6bb57e19176f6cf8aa4
bioinformaticsguy <bioinformaticsguy@gmail.com>
2023-12-27 13:38:45 +0100

    testing the new dataframe function.

--- main_dataframe_preprocessor.py
+++ main_dataframe_preprocessor.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+
+import pandas
+import pandas as pd
+
 from src.dataframe_preprocessor import MaveGoldStandard, MutepredTrainingProcessor, dbNSFPProcessor
 from main import MAVE_GS_FILE_PATH
 from src.utils import COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST
@@ -8,6 +12,7 @@ import copy
 AMINO_ACID_SEQUENCE_COLUMN_NAME = 'Prot_sequence'
 SNP_COLUMN_NAME = "HGVSp_ANNOVAR"
 MUTEPRED_SCORE_COLUMN_NAME = "MutPred_score"
+MAVE_SCORE_COLUMN_NAME = "SNP_dict"
 MUTEPRED_TOOL_NAME = "MutPred"
 TRAINING_DATA_FILE_PATH = Path("Data/mutepred_training_data/wo_exclusive_hgmd_mp2_training_data_MavedbData.csv")
 OUTPUT_DIR_DB_NSFP = Path("Data/dbNSFP_output_dir")
@@ -40,24 +45,45 @@ MAVE_GS_DATAFRAME = dbNSFPProcessor.add_tool_score_column(mave_gs_dataframe=MAVE
                                                     snp_column_name=SNP_COLUMN_NAME)
 
 
-import pandas as pd
 
-# Two sample dictionaries
-dict1 = {'A': 1, 'B': 2, 'C': 3, 'D': 100}
-dict2 = {'A': 10, 'B': 20, 'C': 30}
+def create_mave_tool_scores_dataframe(mave_scores_dict: dict, tool_scores_dict: dict, mave_score_column_name: str,
+                                      tool_score_column_name: str) -> pandas.DataFrame:
+    """
+    Create a DataFrame with MAVE and tool scores.
 
-# Create a set of all keys from both dictionaries
-all_keys = set(dict1.keys()).union(dict2.keys())
+    Parameters:
+    - mave_scores_dict (dict): Dictionary containing MAVE scores.
+    - tool_scores_dict (dict): Dictionary containing tool scores.
+    - mave_score_column_name (str): Name of the column for MAVE scores.
+    - tool_score_column_name (str): Name of the column for tool scores.
 
-# Create a DataFrame with None as default value
-df = pd.DataFrame({key: [dict1.get(key), dict2.get(key)] for key in all_keys},
-                  index=['Dict1_Values', 'Dict2_Values']).T.reset_index()
+    Returns:
+    - pd.DataFrame: DataFrame with 'SNPs', 'MAVE_Scores', and 'Tool_Scores' columns.
+    """
 
-# Rename the columns
-df.columns = ['Keys', 'Dict1_Values', 'Dict2_Values']
+    # Create a set of all keys from both dictionaries
+    all_keys = set(mave_scores_dict.keys()).union(tool_scores_dict.keys())
 
-# Display the DataFrame
-print(df)
+    # Create a DataFrame with None as the default value
+    df = pd.DataFrame({key: [mave_scores_dict.get(key), tool_scores_dict.get(key)] for key in all_keys},
+                      index=[mave_score_column_name, tool_score_column_name]).T.reset_index()
+
+    # Rename the columns
+    df.columns = ['SNPs', mave_score_column_name, tool_score_column_name]
+
+    return df
+
+your_column_values = MAVE_GS_DATAFRAME['protein_name'].tolist()
+
+protein_name = your_column_values[1]
+
+mave_score_dict = MAVE_GS_DATAFRAME.loc[MAVE_GS_DATAFRAME['protein_name'] == protein_name, MAVE_SCORE_COLUMN_NAME].values[0]
+tool_score_dict = MAVE_GS_DATAFRAME.loc[MAVE_GS_DATAFRAME['protein_name'] == protein_name, MUTEPRED_SCORE_COLUMN_NAME].values[0]
 
 
-print("stop")
+df = create_mave_tool_scores_dataframe(mave_score_dict,
+                                       tool_score_dict,
+                                       mave_score_column_name=MAVE_SCORE_COLUMN_NAME,
+                                       tool_score_column_name=MUTEPRED_SCORE_COLUMN_NAME)
+
+print("pause")
