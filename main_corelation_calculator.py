31427356ca6ecb328c10502e6ec21b45ad0da09d
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-01-13 11:27:08 +0100

    Removed extra code in the main_corelation_calculator.py

--- main_corelation_calculator.py
+++ main_corelation_calculator.py
@@ -1,7 +1,7 @@
 
 from src.constants import MAVE_DATAFRAME_PICKLE_FILE_NAME, TOOLS_LIST, PICKLED_DATAFRAMES_DIRECTORY_PATH, \
     MUTEPRED_TOOL_NAME
-from src.utils import load_dataframe, filter_dataframe_by_species, pickle_dataframe, add_missing_columns
+from src.utils import load_dataframe, pickle_dataframe, add_missing_columns
 from src.corelation_calculator import CorelationUpdator
 
 
@@ -10,21 +10,15 @@ if __name__ == '__main__':
     LOADED_MAVE_DF = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
                                         file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
 
-    # ONLY_HUMAN_MAVE_DF = filter_dataframe_by_species(LOADED_MAVE_DF)
-
-
-    ONLY_HUMAN_MAVE_DF_WITH_CORELATIONS = CorelationUpdator. \
+    LOADED_MAVE_DF = CorelationUpdator. \
         add_tool_data_for_multiple_tools(mave_goldstandard_df=LOADED_MAVE_DF,
                                          tools_names_list=TOOLS_LIST)
 
     mutepred_tool_bias = CorelationUpdator. \
-        calculate_tool_bias(df_with_spearman_scores=ONLY_HUMAN_MAVE_DF_WITH_CORELATIONS,
+        calculate_tool_bias(df_with_spearman_scores=LOADED_MAVE_DF,
                             tool_name=MUTEPRED_TOOL_NAME)
 
-    appended_df = add_missing_columns(dataframe1=LOADED_MAVE_DF,
-                                      dataframe2=ONLY_HUMAN_MAVE_DF_WITH_CORELATIONS)
-
-    pickle_dataframe(dataframe=appended_df,
+    pickle_dataframe(dataframe=LOADED_MAVE_DF,
                      file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
                      file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
 
