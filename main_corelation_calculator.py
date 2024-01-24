0fb90602053073b17207ee259db6e8c865c28a7b
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-01-24 01:13:32 +0100

    Updated the function calls according to new functions.

--- main_corelation_calculator.py
+++ main_corelation_calculator.py
@@ -9,13 +9,22 @@ if __name__ == '__main__':
                                         file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
 
     LOADED_MAVE_DF = CorelationUpdator. \
-        add_tool_data_for_multiple_tools(mave_goldstandard_df=LOADED_MAVE_DF,
+        add_tool_data_for_multiple_tools(LOADED_MAVE_DF,
                                          tools_names_list=TOOLS_LIST)
 
-    mutepred_tool_bias = CorelationUpdator. \
-        calculate_tool_bias(df_with_spearman_scores=LOADED_MAVE_DF,
-                            tool_name=MUTEPRED_TOOL_NAME)
 
+    LOADED_MAVE_DF = CorelationUpdator. \
+        add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df=LOADED_MAVE_DF,
+                                                       tool_name=MUTEPRED_TOOL_NAME,
+                                                       exclude_tool_training_snps_flag=True)
+
+
+
+    #
+    # mutepred_tool_bias = CorelationUpdator. \
+    #     calculate_tool_bias(df_with_spearman_scores=LOADED_MAVE_DF,
+    #                         tool_name=MUTEPRED_TOOL_NAME)
+    #
     pickle_dataframe(dataframe=LOADED_MAVE_DF,
                      file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
                      file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
