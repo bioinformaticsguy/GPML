0566e91abd1b9fb4144ad974ca0885ce8bc47ca9
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-02-13 17:59:28 +0100

    Updated and added the function call.

--- main_plot_graphs.py
+++ main_plot_graphs.py
@@ -1,44 +1,15 @@
 from src.constants import MAVE_DATAFRAME_PICKLE_FILE_NAME, PICKLED_DATAFRAMES_DIRECTORY_PATH, PEARSON_CORELATION_SUFFIX, \
     PROTEIN_SHORT_MAPPING
 from src.utils import load_dataframe, filter_dataframe_by_species
-from itertools import cycle
-import matplotlib.pyplot as plt
+from src.plot_graphs import PlotGeneroator
 
 if __name__ == '__main__':
     LOADED_MAVE_DF = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
                                         file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
 
-    LOADED_MAVE_DF = filter_dataframe_by_species(LOADED_MAVE_DF)
+    ONLY_HUMAN_DATABASE = filter_dataframe_by_species(LOADED_MAVE_DF)
 
-    # Sample data
-    df = LOADED_MAVE_DF
+    PlotGeneroator.plot_correlations(ONLY_HUMAN_DATABASE, PROTEIN_SHORT_MAPPING, PEARSON_CORELATION_SUFFIX)
 
-    columns_list = df.columns.tolist()
-    filtered_columns_list = [column for column in columns_list if column.endswith(PEARSON_CORELATION_SUFFIX)]
-
-    legend = [value.rstrip(PEARSON_CORELATION_SUFFIX) for value in filtered_columns_list]
-
-    x = [PROTEIN_SHORT_MAPPING[protein_name] for protein_name in df['protein_name'].tolist()]
-
-    # Generating dynamic colors based on the number of legend entries
-    num_colors = len(legend)
-    color_cycle = cycle(plt.cm.get_cmap('tab10').colors)
-    colors = [next(color_cycle) for _ in range(num_colors)]
-
-    fig, ax = plt.subplots()
-
-    for column, label, color in zip(filtered_columns_list, legend, colors):
-        y = df[column].abs().tolist()
-        ax.scatter(x, y, label=label, color=color, linewidth=4)
-
-    ax.set_xlabel('Protein Names', fontsize=14)
-    ax.set_ylabel('Correlations', fontsize=14)
-    ax.legend(title='Tool Name', fontsize=12, labels=legend)
-    ax.set_xticks(x)
-    ax.set_xticklabels(x, rotation=45, fontsize=14)
-    fig.set_size_inches(20, 20)
-    fig.subplots_adjust(bottom=0.1)
-
-    plt.show()
 
     print("Debug Pause")
