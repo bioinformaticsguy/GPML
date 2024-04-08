d9bb86011f979c0971dc67b8517ef5bd09d26461
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-04-08 07:58:04 +0200

    Loaded the only_human_df directly

--- main_baseline_calculation.py
+++ main_baseline_calculation.py
@@ -1,11 +1,10 @@
-from src.constants import PICKLED_DATAFRAMES_DIRECTORY_PATH, MAVE_DATAFRAME_PICKLE_FILE_NAME, SPECIE_NAME_HUMAN
-from src.utils import load_dataframe, filter_dataframe_by_species
+from src.constants import PICKLED_DATAFRAMES_DIRECTORY_PATH, MAVE_DATAFRAME_PICKLE_FILE_NAME, SPECIE_NAME_HUMAN, \
+    MAVE_DATAFRAME_ONLY_HUMAN_PICKLE_FILE_NAME
+from src.utils import load_dataframe, filter_dataframe_by_species, pickle_dataframe
 
 if __name__ == '__main__':
-    LOADED_MAVE_DF = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
-                                        file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
+    GOLD_STD_DF_ONLY_HUMAN = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
+                                 file_name=MAVE_DATAFRAME_ONLY_HUMAN_PICKLE_FILE_NAME)
 
-    ONLY_HUMAN_DF = filter_dataframe_by_species(df=LOADED_MAVE_DF,
-                                                target_species=SPECIE_NAME_HUMAN)
 
     print("Debug Pause")
