98c3dbdf312c6f8b6284c9b3242196cfe23a2ffb
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-04-12 15:13:36 +0200

    removed non usable code.

--- testing_code.py
+++ testing_code.py
@@ -1,13 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 
 
-
-# Example usage:
-data = {
-    'Category': ['A', 'B', 'A', 'C', 'B', 'A', 'A', 'B', 'C', 'C'],
-    'Value': [10, 20, 15, 25, 30, 12, 18, 22, 28, 35]
-}
-df = pd.DataFrame(data)
-
-plot_pie_with_counts(df, 'Category')
+print("debug pause")
