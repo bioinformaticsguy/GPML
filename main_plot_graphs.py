a7e83b1c4e3b801a054efb23f47a5c055ebb3b1d
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-05-12 22:32:51 +0200

    chore: Update file path for loading MAVE dataframe

--- main_plot_graphs.py
+++ main_plot_graphs.py
@@ -5,15 +5,15 @@ from src.plot_graphs import PlotGeneroator
 
 if __name__ == '__main__':
     LOADED_MAVE_DF = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
-                                        file_name=MAVE_DATAFRAME_PICKLE_FILE_NAME)
+                                        file_name="pssm_base.pkl")
 
-    ONLY_HUMAN_DATABASE = filter_dataframe_by_species(LOADED_MAVE_DF)
+    # ONLY_HUMAN_DATABASE = filter_dataframe_by_species(LOADED_MAVE_DF)
 
     # ONLY_HUMAN_DATABASE = ONLY_HUMAN_DATABASE.loc[ONLY_HUMAN_DATABASE['protein_name'] != 'CYP2C9_urn:mavedb:00000095-a']
 
-    ONLY_HUMAN_DATABASE.fillna(0, inplace=True)
+    LOADED_MAVE_DF.fillna(0, inplace=True)
 
-    PlotGeneroator.plot_correlations(ONLY_HUMAN_DATABASE, PROTEIN_SHORT_MAPPING, PEARSON_CORELATION_SUFFIX)
+    PlotGeneroator.plot_correlations(LOADED_MAVE_DF, PROTEIN_SHORT_MAPPING, PEARSON_CORELATION_SUFFIX)
 
 
     # PlotGeneroator.plot_pie_with_counts(LOADED_MAVE_DF, "species")
