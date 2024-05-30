72a6dbf938aee23c5f5f2e98294920582a6be7d4
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-05-30 17:50:40 +0200

    Testing data with uniprot column.

--- main_dataframe_preprocessor.py
+++ main_dataframe_preprocessor.py
@@ -28,29 +28,34 @@ def add_data_from_list_of_tools(mave_gs_dataframe,
     return mave_gs_dataframe
 
 if __name__ == '__main__':
-    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
-                                                           file_name=MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_PICKLE_FILE_NAME)
+    MAVE_GS_DATAFRAME = MaveGoldStandard.get_dataframe_for_mave_gs_data(mave_gs_file_path=MAVE_GS_FILE_PATH,
+                                                                        column_names=COLUMN_NAMES_OF_MAVE_GS_DATAFRAME_LIST)
 
+    # MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
+    #                                                        file_name=MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_PICKLE_FILE_NAME)
+    #
     MUTEPRED_DATAFRAME = MutepredTrainingProcessor.get_mutepred_df(TRAINING_DATA_FILE_PATH)
     MUTEPRED_DATAFRAME = convert_column_to_list(MUTEPRED_DATAFRAME, MUTEPRED_AMINO_ACID_SUBSTITUTIONS_COLUMN_NAME)
 
-    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = add_column_from_tool_df_to_mave_df(mave_df=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
+    MAVE_GS_DATAFRAME = add_column_from_tool_df_to_mave_df(mave_df=MAVE_GS_DATAFRAME,
                                                            tool_df=MUTEPRED_DATAFRAME,
                                                            mave_df_prot_seq_col_name=COLUMN_NAME_OF_MAVE_GOLD_STANDARD_PROTEIN_SEQUENCE,
                                                            tool_df_prot_seq_col_name=AMINO_ACID_SEQUENCE_COLUMN_NAME,
                                                            tool_col_to_add=MUTEPRED_AMINO_ACID_SUBSTITUTIONS_COLUMN_NAME,
                                                            name_of_new_col=MUTEPRED_TRAINING_SNPS_COLUMN_NAME)
 
-    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = add_flag_column(df=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
+    MAVE_GS_DATAFRAME = add_flag_column(df=MAVE_GS_DATAFRAME,
                                         target_column=MUTEPRED_TRAINING_SNPS_COLUMN_NAME,
                                         flag_column_name=MUTEPRED_TRAINING_FLAG_COLUMN_NAME)
 
-    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = add_data_from_list_of_tools(MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
+    MAVE_GS_DATAFRAME = add_data_from_list_of_tools(MAVE_GS_DATAFRAME,
                                                                         db_nsfp_output_dir_path=OUTPUT_DIR_DB_NSFP,
                                                                         tool_list=TOOLS_LIST,
                                                                         snp_column_name=SNP_COLUMN_NAME)
-    pickle_dataframe(dataframe=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
-                     file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
-                     file_name=MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_PICKLE_FILE_NAME)
+
+
+    # pickle_dataframe(dataframe=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
+    #                  file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
+    #                  file_name=MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_PICKLE_FILE_NAME)
 
     print("Debug Pause")
