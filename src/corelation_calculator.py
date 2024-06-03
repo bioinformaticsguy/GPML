33acd1f57ef848299f98136f27aa99acc4a898cd
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-06-03 18:06:50 +0200

    Impleamntation of the function add_deogen_baseline_corelation

--- src/corelation_calculator.py
+++ src/corelation_calculator.py
@@ -1,5 +1,6 @@
 import numpy as np
-from src.constants import PEARSON_CORELATION_SUFFIX, TRAINING_FLAG_SUFFIX, TOOLS_LIST
+from src.constants import PEARSON_CORELATION_SUFFIX, TRAINING_FLAG_SUFFIX, TOOLS_LIST, \
+    COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY
 from src.dataframe_preprocessor import COLUMN_NAME_OF_MAVE_GOLD_STANDARD_ID, \
     COLUMN_NAME_OF_MAVE_GOLD_STANDARD_SNP_DICTIONARY
 from src.utils import get_mave_tool_scores_dataframe, get_correlation_and_percentage_used, exclude_snps, \
@@ -10,7 +11,7 @@ class CorelationUpdator:
     @staticmethod
     def add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df,
                                                        tool_name,
-                                                       exclude_tool_training_snps_flag = False,
+                                                       exclude_tool_training_snps_flag=False,
                                                        mave_df_id_column_name=COLUMN_NAME_OF_MAVE_GOLD_STANDARD_ID,
                                                        mave_score_dict_column_name=COLUMN_NAME_OF_MAVE_GOLD_STANDARD_SNP_DICTIONARY):
         """
@@ -30,8 +31,6 @@ class CorelationUpdator:
         # Define column names for tool-specific scores
         tool_pearson_score_column, tool_snps_percentage_column, tool_score_column_name = \
             generate_tool_columns(tool_name=tool_name, exclude_tool_training_snps_flag=exclude_tool_training_snps_flag)
-
-
 
         # Initialize new columns with None
         mave_goldstandard_df[tool_pearson_score_column] = np.NaN
@@ -63,7 +62,6 @@ class CorelationUpdator:
                                                               protein_name, training_snps_column_name].iloc[0]
 
                 mave_tool_scores_df = exclude_snps(df=mave_tool_scores_df, exclude_snps_list=training_snps_list)
-
 
             # Calculate Pearson correlation and percentage of used rows
             used_rows_percentage, pearson_correlation = get_correlation_and_percentage_used(
@@ -125,17 +123,49 @@ class CorelationUpdator:
             for tool_name in tools_names_list:
                 mave_goldstandard_df = CorelationUpdator. \
                     add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df=mave_goldstandard_df,
-                                                               tool_name=tool_name,
-                                                               exclude_tool_training_snps_flag=True)
+                                                                   tool_name=tool_name,
+                                                                   exclude_tool_training_snps_flag=True)
         else:
             for tool_name in tools_names_list:
                 mave_goldstandard_df = CorelationUpdator. \
                     add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df=mave_goldstandard_df,
-                                                               tool_name=tool_name)
+                                                                   tool_name=tool_name)
 
         return mave_goldstandard_df
 
+
 class DeogenCorelation:
     @staticmethod
-    def add_deogen_baseline_corelation():
-        pass
+    def add_deogen_baseline_corelation(mave_gs_df,
+                                       baseline_score_col_name=COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY,
+                                       mave_df_id_column_name=COLUMN_NAME_OF_MAVE_GOLD_STANDARD_ID,
+                                       mave_score_dict_column_name=COLUMN_NAME_OF_MAVE_GOLD_STANDARD_SNP_DICTIONARY,
+                                       pearson_corelation_suffix=PEARSON_CORELATION_SUFFIX, ):
+
+
+        baseline_pearson_score_col = baseline_score_col_name + pearson_corelation_suffix
+        mave_gs_df[baseline_pearson_score_col] = np.NaN
+
+        protein_names = mave_gs_df[mave_df_id_column_name].tolist()
+
+        for protein_name in protein_names:
+            # Extract MAVE and tool-specific scores dictionaries for the current protein
+            mave_score_dict = mave_gs_df.loc[
+                mave_gs_df[mave_df_id_column_name] == protein_name, mave_score_dict_column_name].values[0]
+            baeline_score_dict = mave_gs_df.loc[
+                mave_gs_df[mave_df_id_column_name] == protein_name, baseline_score_col_name].values[0]
+
+            mave_tool_scores_df = get_mave_tool_scores_dataframe(mave_score_dict,
+                                                                 baeline_score_dict,
+                                                                 mave_score_dictionary_column_name=mave_score_dict_column_name,
+                                                                 tool_score_dictionary_column_name=baseline_score_col_name)
+
+            used_rows_percentage, pearson_correlation = get_correlation_and_percentage_used(
+                df=mave_tool_scores_df,
+                column1_name=mave_score_dict_column_name,
+                column2_name=baseline_score_col_name)
+
+            mave_gs_df.loc[mave_gs_df[mave_df_id_column_name] == \
+                                    protein_name, baseline_pearson_score_col] = pearson_correlation
+
+        return mave_gs_df
