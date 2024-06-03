6cf7a4a2ffc378c2471fe2f6dae0e97457f00b37
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-06-03 21:43:23 +0200

    Code refractoring and also saving the files now.

--- main_plot_graphs.py
+++ main_plot_graphs.py
@@ -1,11 +1,28 @@
+from pathlib import Path
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from src.constants import PICKLED_DATAFRAMES_DIRECTORY_PATH, \
     MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_CORELATION_PICKLE_FILE_NAME, PROTEIN_SHORT_DICTMAP, MUTEPRED_TOOL_NAME, \
-    COLUMN_NAME_OF_MAVE_GOLD_STANDARD_ID, COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY, PEARSON_CORELATION_SUFFIX
+    COLUMN_NAME_OF_MAVE_GOLD_STANDARD_ID, COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY, PEARSON_CORELATION_SUFFIX, \
+    DEOGEN_TOOL_NAME, EXCLUDE_TRAINING_SNP_SUFFIX, TOOLS_LIST, PLOTS_DIRECTORY_PATH
 from src.utils import load_dataframe
 from src.plot_graphs import PlotGeneroator
+
+
+def generate_plot(tool_name, file_name, exclude_training_snps=False):
+    column1 = tool_name + PEARSON_CORELATION_SUFFIX
+    if exclude_training_snps:
+        column1 += EXCLUDE_TRAINING_SNP_SUFFIX
+    column2 = column1 + EXCLUDE_TRAINING_SNP_SUFFIX
+
+    column_names = [id_column, baseline_column, column1, column2]
+    sorted_df = LOADED_MAVE_DF.loc[:, column_names].sort_values(by=baseline_column, ascending=True)
+
+    protein_names = [PROTEIN_SHORT_DICTMAP[name] for name in sorted_df.pop(id_column).tolist()]
+    dict_df = {k.replace(PEARSON_CORELATION_SUFFIX, ""): v for k, v in sorted_df.abs().to_dict('list').items()}
+    PlotGeneroator.generate_bar_plot(protein_names, dict_df, file_name,)
 
 if __name__ == '__main__':
     LOADED_MAVE_DF = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
@@ -17,15 +34,35 @@ if __name__ == '__main__':
 
 
     id_column = "protein_name"
-    baseline_column  = "pssmBaseline_pearson_correlation"
-    column1 = "DEOGEN2_pearson_correlation"
-    column2 = "DEOGEN2_pearson_correlation_excluded_training_snps"
+    baseline_column  = COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY + PEARSON_CORELATION_SUFFIX
+
+    # # Deogen Plot
+    file_name = PLOTS_DIRECTORY_PATH / Path(DEOGEN_TOOL_NAME + ".svg")
+    generate_plot(DEOGEN_TOOL_NAME, file_name)
+
+    # # # MutPred Plot
+    file_name = PLOTS_DIRECTORY_PATH / Path(MUTEPRED_TOOL_NAME + ".svg")
+    generate_plot(MUTEPRED_TOOL_NAME, file_name)
+
+    # # # Both Tools Plot Exclude Training SNPs
+    file_name = PLOTS_DIRECTORY_PATH / Path(MUTEPRED_TOOL_NAME + DEOGEN_TOOL_NAME + ".svg")
+    column1 = MUTEPRED_TOOL_NAME + PEARSON_CORELATION_SUFFIX + EXCLUDE_TRAINING_SNP_SUFFIX
+    column2 = DEOGEN_TOOL_NAME + PEARSON_CORELATION_SUFFIX + EXCLUDE_TRAINING_SNP_SUFFIX
 
     column_names = [id_column, baseline_column, column1, column2]
     sorted_df = LOADED_MAVE_DF.loc[:, column_names].sort_values(by=baseline_column, ascending=True)
 
     protein_names = [PROTEIN_SHORT_DICTMAP[name] for name in sorted_df.pop(id_column).tolist()]
-    dict_df = {k.replace("_pearson_correlation", ""): v for k, v in sorted_df.abs().to_dict('list').items()}
-    PlotGeneroator.generate_bar_plot(protein_names, dict_df)
+    dict_df = {k.replace(PEARSON_CORELATION_SUFFIX, ""): v for k, v in sorted_df.abs().to_dict('list').items()}
+    PlotGeneroator.generate_bar_plot(protein_names, dict_df, file_name)
+    #
+    # # # All Tools Plot Exclude Training SNPs
+    file_name = PLOTS_DIRECTORY_PATH / Path("all_tools" + ".svg")
+    column_names = [id_column, baseline_column] + [tool + PEARSON_CORELATION_SUFFIX for tool in TOOLS_LIST]
+    sorted_df = LOADED_MAVE_DF.loc[:, column_names].sort_values(by=baseline_column, ascending=True)
+
+    protein_names = [PROTEIN_SHORT_DICTMAP[name] for name in sorted_df.pop(id_column).tolist()]
+    dict_df = {k.replace(PEARSON_CORELATION_SUFFIX, ""): v for k, v in sorted_df.abs().to_dict('list').items()}
+    PlotGeneroator.generate_bar_plot(protein_names, dict_df, file_name, height = 0.14)
 
     print("Debug Pause")
