5252ba7188f39c02053a090411ad2241815988b7
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-06-04 00:15:43 +0200

    generated the script to run all the files in the pipline.

---
+++ all_runner.py
@@ -0,0 +1,10 @@
+import subprocess
+
+# List of your scripts
+scripts = ["main_dataframe_preprocessor.py",
+           "main_pssm_baseline.py",
+           "main_corelation_calculator.py",
+           "main_plot_graphs.py"]
+
+for script in scripts:
+    subprocess.call(["python", script])
