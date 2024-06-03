812b0ad2080d091628bb0ae4a84c01cde1054eba
bioinformaticsguy <bioinformaticsguy@gmail.com>
2024-06-03 18:06:28 +0200

    updated the code to pickle the dataframe with the baseline corelations.

--- main_corelation_calculator.py
+++ main_corelation_calculator.py
@@ -1,9 +1,8 @@
 from src.constants import MAVE_DATAFRAME_PICKLE_FILE_NAME, TOOLS_LIST, PICKLED_DATAFRAMES_DIRECTORY_PATH, \
     MUTEPRED_TOOL_NAME, MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_PICKLE_FILE_NAME, \
     COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY, MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_CORELATION_PICKLE_FILE_NAME
-from src.utils import load_dataframe, pickle_dataframe
-from src.corelation_calculator import CorelationUpdator
-
+from src.utils import load_dataframe, pickle_dataframe, filter_dataframe_by_species
+from src.corelation_calculator import CorelationUpdator, DeogenCorelation
 
 if __name__ == '__main__':
     MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = load_dataframe(file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
@@ -14,25 +13,20 @@ if __name__ == '__main__':
         add_tool_data_for_multiple_tools(MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
                                          tools_names_list=TOOLS_LIST)
 
-
-    LOADED_MAVE_DF = CorelationUpdator. \
+    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = CorelationUpdator. \
         add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
                                                        tool_name=MUTEPRED_TOOL_NAME,
                                                        exclude_tool_training_snps_flag=True)
 
-    LOADED_MAVE_DF = CorelationUpdator. \
-        add_tool_correlation_and_snp_percentage_column(mave_goldstandard_df=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
-                                                       tool_name=COLUMN_NAME_OF_BASELINE_SCORES_DICTIONARY,)
+    MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE = DeogenCorelation.add_deogen_baseline_corelation(MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,)
 
 
+    mean_normal = MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE["MutPred_pearson_correlation"].mean()
+
+    mean_no_bias = MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE["MutPred_pearson_correlation_excluded_training_snps"].mean()
 
 
-    mean_normal = LOADED_MAVE_DF["MutPred_pearson_correlation"].mean()
-
-    mean_no_bias = LOADED_MAVE_DF["MutPred_pearson_correlation_excluded_training_snps"].mean()
-
-
-    strict = CorelationUpdator.calculate_tool_bias(df_with_spearman_scores=LOADED_MAVE_DF,
+    strict = CorelationUpdator.calculate_tool_bias(df_with_spearman_scores=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
                         tool_name=MUTEPRED_TOOL_NAME)
 
     relax = abs(mean_no_bias - mean_normal)
@@ -42,9 +36,10 @@ if __name__ == '__main__':
     #     calculate_tool_bias(df_with_spearman_scores=LOADED_MAVE_DF,
     #                         tool_name=MUTEPRED_TOOL_NAME)
     #
-    pickle_dataframe(dataframe=LOADED_MAVE_DF,
+    pickle_dataframe(dataframe=MAVE_GS_DATAFRAME_HUMAN_WITH_BASELINE,
                      file_path=PICKLED_DATAFRAMES_DIRECTORY_PATH,
                      file_name=MAVE_DATAFRAME_ONLY_HUMAN_WITH_BASELINE_CORELATION_PICKLE_FILE_NAME)
+
 
 
     print("Debug Pause")
